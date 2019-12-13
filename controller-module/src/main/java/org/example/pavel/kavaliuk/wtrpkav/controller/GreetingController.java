39c3ac2eb6f46dbf0a7c9c56f5f86f1a0c8cd7c2
wondercat <wondercat365@mail.ru>
2019-12-13 13:40:35 +0300

    #1: Project is multi-modular now. Added greeting controller.

---
+++ controller-module/src/main/java/org/example/pavel/kavaliuk/wtrpkav/controller/GreetingController.java
@@ -0,0 +1,11 @@
+package org.example.pavel.kavaliuk.wtrpkav.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+public class RestController {
+    @PostMapping
+    public String 
+}
