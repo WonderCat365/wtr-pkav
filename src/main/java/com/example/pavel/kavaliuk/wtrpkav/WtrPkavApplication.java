5e5d3c93d9a0bcbec8b3c57bc5bacb4462651aa5
wondercat <wondercat365@mail.ru>
2019-12-13 12:41:25 +0300

    Initial commit

---
+++ src/main/java/com/example/pavel/kavaliuk/wtrpkav/WtrPkavApplication.java
@@ -0,0 +1,13 @@
+package com.example.pavel.kavaliuk.wtrpkav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtrPkavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtrPkavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pavel/kavaliuk/wtrpkav/WtrPkavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pavel.kavaliuk.wtrpkav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtrPkavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
